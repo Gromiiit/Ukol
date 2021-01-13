26722554f5f28f83871ea15b4b0c2d5a95d89935
Gromovla <vladimirgromotovic@gmail.com>
2021-01-13 09:44:45 +0100

    Possible issues

--- Ukol/Program.cs
+++ Ukol/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Xml.Linq;
 using Newtonsoft.Json;
 
@@ -22,26 +23,28 @@ namespace Notino.Homework
             try
             {
                 FileStream sourceStream = File.Open(sourceFileName, FileMode.Open);
-                var reader = new StreamReader(sourceStream);
-                string input = reader.ReadToEnd();
+                var reader = new StreamReader(sourceStream); // encoding of sourceFile is always represented as UTF-8 (sourceFile may be in UTF32, ANSI, etc) => possible loss of data
+                string input = reader.ReadToEnd(); // possible out of memory exception on large files
+                // streams are not closed => can cause multiple issues
             }
-            catch (Exception ex)
+            catch (Exception ex) // general exception handling
             {
                 throw new Exception(ex.Message);
             }
 
-            var xdoc = XDocument.Parse(input);
+            var xdoc = XDocument.Parse(input); // input is unknown in this context (Compile-time error), possible exception on wrong format of xml file
             var doc = new Document
             {
-                Title = xdoc.Root.Element("title").Value,
-                Text = xdoc.Root.Element("text").Value
+                Title = xdoc.Root.Element("title").Value, // possible null reference exception if element is not found
+                Text = xdoc.Root.Element("text").Value // possible null reference exception if element is not found
             };
 
             var serializedDoc = JsonConvert.SerializeObject(doc);
 
-            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write); // possible directory not found exception, unauthorize access exception, possible override of already existing file (bug or feature?)
             var sw = new StreamWriter(targetStream);
             sw.Write(serializedDoc);
+            // streams are not closed => can cause multiple issues
         }
     }
 }
