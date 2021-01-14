91d4525e296a89264781f2072270490b17919c42
Gromovla <vladimirgromotovic@gmail.com>
2021-01-14 09:45:33 +0100

    Xml unit tests

--- ConvertorsTest/ConvertorsTest.cs
+++ ConvertorsTest/ConvertorsTest.cs
@@ -12,7 +12,7 @@ namespace Notino.Homework.ConvertorsTest
     {
         public static string DeleteControls(this string input)
         {
-            return new string(input.Where(c => !char.IsControl(c)).ToArray());
+            return new string(input.Where(c => !char.IsControl(c) && !char.IsWhiteSpace(c)).ToArray());
         }
     }
     [TestClass]
@@ -163,5 +163,164 @@ namespace Notino.Homework.ConvertorsTest
             Assert.ThrowsException<SerializationException>(() => convertor.Serialize<Document>(anon));
         }
 
+        /////////////////
+        [TestMethod]
+        public void XmlDeserializeObject()
+        {
+            var seriallized = "<Document><Title>title</Title><Text>text</Text></Document>";
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new XMLConvertor();
+            var deseriallized = convertor.Deserialize<Document>(seriallized).ToList().ElementAt(0);
+
+            Assert.AreEqual(document.Title, deseriallized.Title);
+            Assert.AreEqual(document.Text, deseriallized.Text);
+        }
+
+        [TestMethod]
+        public void XmlDeserializeList()
+        {
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"<ArrayOfDocument>");
+            stringBuilder.Append(@"<Document><Title>title0</Title><Text>text0</Text></Document>");
+            stringBuilder.Append(@"<Document><Title>title1</Title><Text>text1</Text></Document>");
+            stringBuilder.Append(@"</ArrayOfDocument>");
+
+            var seriallized = stringBuilder.ToString();
+            var document0 = new Document()
+            {
+                Title = "title0",
+                Text = "text0"
+            };
+            var document1 = new Document()
+            {
+                Title = "title1",
+                Text = "text1"
+            };
+            var documentList = new List<Document>() { document0, document1 };
+
+            var convertor = new XMLConvertor();
+            var deseriallized = convertor.Deserialize<Document>(seriallized).ToList();
+
+            for (int i = 0; i < documentList.Count; i++)
+            {
+                Assert.AreEqual(documentList.ElementAt(i).Title, deseriallized.ElementAt(i).Title);
+                Assert.AreEqual(documentList.ElementAt(i).Text, deseriallized.ElementAt(i).Text);
+            }
+            Assert.AreEqual(documentList.Count, deseriallized.Count);
+        }
+
+        [TestMethod]
+        public void XmlSerializeObject()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"<?xml version=""1.0"" encoding=""utf-16""?>");
+            stringBuilder.Append(@"<Document xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">");
+            stringBuilder.Append(@"<Title>title</Title><Text>text</Text>");
+            stringBuilder.Append(@"</Document>");
+
+            var seriallized = stringBuilder.ToString();
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new XMLConvertor();
+            var deseriallized = convertor.Serialize<Document>(document);
+
+            Assert.AreEqual(seriallized.ToLower().DeleteControls(), deseriallized.ToLower().DeleteControls());
+        }
+
+        [TestMethod]
+        public void XmlSerializeList()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"<?xml version=""1.0"" encoding=""utf-16""?>");
+            stringBuilder.Append(@"<ArrayOfDocument xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">");
+            stringBuilder.Append(@"<Document><Title>title0</Title><Text>text0</Text></Document>");
+            stringBuilder.Append(@"<Document><Title>title1</Title><Text>text1</Text></Document>");
+            stringBuilder.Append(@"</ArrayOfDocument>");
+
+            var seriallized = stringBuilder.ToString();
+
+
+            var document0 = new Document()
+            {
+                Title = "title0",
+                Text = "text0"
+            };
+            var document1 = new Document()
+            {
+                Title = "title1",
+                Text = "text1"
+            };
+            var documentList = new List<Document>() { document0, document1 };
+
+            var convertor = new XMLConvertor();
+            var deseriallized = convertor.Serialize<Document>(documentList);
+
+            Assert.AreEqual(seriallized.ToLower().DeleteControls(), deseriallized.ToLower().DeleteControls());
+        }
+
+        [TestMethod]
+        public void XmlDeserializeFromStream()
+        {
+            var seriallized = "<Document><Title>title</Title><Text>text</Text></Document>";
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new XMLConvertor();
+
+            using (var memStream = new MemoryStream(Encoding.UTF8.GetBytes(seriallized)))
+            {
+
+                var deseriallized = convertor.Deserialize<Document>(new StreamReader(memStream)).ToList().ElementAt(0);
+
+                Assert.AreEqual(document.Title, deseriallized.Title);
+                Assert.AreEqual(document.Text, deseriallized.Text);
+            }
+        }
+
+        [TestMethod]
+        public void XmlSerializeToStream()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            stringBuilder.Append(@"<Document xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">");
+            stringBuilder.Append(@"<Title>title</Title><Text>text</Text>");
+            stringBuilder.Append(@"</Document>");
+
+            var seriallized = stringBuilder.ToString();
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new XMLConvertor();
+
+            using (var memStream = new MemoryStream())
+            {
+                convertor.Serialize<Document>(document, new StreamWriter(memStream));
+                string actual = Encoding.UTF8.GetString(memStream.ToArray());
+                System.Console.WriteLine(actual.DeleteControls());
+                System.Console.WriteLine(seriallized.DeleteControls());
+                Assert.AreEqual(seriallized.ToLower().DeleteControls(), actual.ToLower().DeleteControls());
+            }
+        }
+
+        [TestMethod]
+        public void XmlSerializeNotSerializableException()
+        {
+            var anon = new { Text = "title", Title = "text" };
+            var convertor = new XMLConvertor();
+
+            Assert.ThrowsException<SerializationException>(() => convertor.Serialize<Document>(anon));
+        }
     }
 }
