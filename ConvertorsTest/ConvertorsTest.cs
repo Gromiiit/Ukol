69c1bce9bc5aa07b67a5e578a9c8bb78a332f92b
Gromovla <vladimirgromotovic@gmail.com>
2021-01-14 08:30:40 +0100

    Json unit tests

--- ConvertorsTest/ConvertorsTest.cs
+++ ConvertorsTest/ConvertorsTest.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Notino.Homework.FormatConvertors;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
 
 namespace Notino.Homework.ConvertorsTest
 {
+    public static class StringMod
+    {
+        public static string DeleteControls(this string input)
+        {
+            return new string(input.Where(c => !char.IsControl(c)).ToArray());
+        }
+    }
     [TestClass]
     public class ConvertorsTest
     {
+        
+
         [TestMethod]
         public void FactoryJsonConvertor()
         {
@@ -77,7 +89,7 @@ namespace Notino.Homework.ConvertorsTest
             var convertor = new JSONConvertor();
             var deseriallized = convertor.Serialize<Document>(document);
 
-            Assert.AreEqual(seriallized.ToLower().Replace(" ", ""), deseriallized.ToLower().Replace(" ", ""));
+            Assert.AreEqual(seriallized.ToLower().DeleteControls(), deseriallized.ToLower().DeleteControls());
         }
 
         [TestMethod]
@@ -99,7 +111,57 @@ namespace Notino.Homework.ConvertorsTest
             var convertor = new JSONConvertor();
             var deseriallized = convertor.Serialize<Document>(documentList);
 
-            Assert.AreEqual(seriallized.ToLower().Replace(" ", ""), deseriallized.ToLower().Replace(" ", ""));
+            Assert.AreEqual(seriallized.ToLower().DeleteControls(), deseriallized.ToLower().DeleteControls());
         }
+
+        [TestMethod]
+        public void JsonDeserializeFromStream()
+        {
+            var seriallized = "{\"title\":\"title\",\"text\":\"text\"}";
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new JSONConvertor();
+
+            using (var memStream = new MemoryStream(Encoding.UTF8.GetBytes(seriallized)))
+            {
+                
+                var deseriallized = convertor.Deserialize<Document>(new StreamReader(memStream)).ToList().ElementAt(0);
+
+                Assert.AreEqual(document.Title, deseriallized.Title);
+                Assert.AreEqual(document.Text, deseriallized.Text);
+            }
+        }
+
+        [TestMethod]
+        public void JsonSerializeToStream()
+        {
+            var seriallized = "{\"title\":\"title\",\"text\":\"text\"}";
+            var document = new Document()
+            {
+                Title = "title",
+                Text = "text"
+            };
+            var convertor = new JSONConvertor();
+
+            using (var memStream = new MemoryStream())
+            {
+                convertor.Serialize<Document>(document, new StreamWriter(memStream));
+                string actual = Encoding.UTF8.GetString(memStream.ToArray());
+                Assert.AreEqual(seriallized.ToLower().DeleteControls(), actual.ToLower().DeleteControls());
+            }
+        }
+
+        [TestMethod]
+        public void JsonSerializeNotSerializableException()
+        {
+            var anon = new { Text = "title", Title = "text" };
+            var convertor = new JSONConvertor();
+            
+            Assert.ThrowsException<SerializationException>(() => convertor.Serialize<Document>(anon));
+        }
+
     }
 }
